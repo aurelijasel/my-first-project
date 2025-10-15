894cc94c198030edf1682891eb4547eeaeee127a
aurelijasel <aurelija.seliukaite@gmail.com>
2025-10-15 13:00:26 +0300

    Update duomenuoperacijos.h

--- duomenuoperacijos.h
+++ duomenuoperacijos.h
@@ -4,9 +4,11 @@
 #include "studentas.h"
 #include <string>
 #include <vector>
+#include <list>
 
 std::vector<Studentas> nuskaityti(std::string failas);
 void generuotitxt(const std::string& pavadinimas, int kiekis);
 void suskirstyti(const std::vector<Studentas>& Grupe);
+void suskirstyti(const std::list<Studentas>& Grupe);
 
 #endif
